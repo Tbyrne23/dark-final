7a4710e02f88e7c8713cc09e116496cb4887ae59
Tbyrne23 <81589289+Tbyrne23@users.noreply.github.com>
2022-05-09 13:49:58 -0400

    Add files via upload

---
+++ sponsor.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./sponsor.css";
+
+const Sponsors = () => {
+  return (
+    <div className='Sponsor'>
+        <h1>Cereal Flakes, “Asbestos free”</h1>
+        <h2>Jim’s Brake Pads, “They work every time, 50% of the time”</h2>
+        
+    </div>
+  );
+};
+  
+export default Sponsors;
