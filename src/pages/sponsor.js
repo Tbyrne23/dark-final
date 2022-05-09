ec8bab7c468c2e223f929e530b92ca3735f48a67
Tbyrne23 <81589289+Tbyrne23@users.noreply.github.com>
2022-05-09 15:33:49 -0400

    Add files via upload

--- src/pages/sponsor.js
+++ src/pages/sponsor.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import "./sponsor.css";
-
+import coins from "./pics/istockphoto-1199436529-612x612.jpg";
 const Sponsors = () => {
   return (
     <div className='Sponsor'>
+      
         <h1>Cereal Flakes, “Asbestos free”</h1>
         <h2>Jim’s Brake Pads, “They work every time, 50% of the time”</h2>
-        
+        <img alt='coins' className='photo' src={coins}/>
     </div>
   );
 };
